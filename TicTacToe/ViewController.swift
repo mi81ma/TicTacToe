b4afc0dc0021f81959e06660b7e8c62af4b7bca6
mi81ma <mi81ma@gmail.com>
2019-01-18 19:15:23 +0800

    Initial Commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by masato on 18/1/2019.
+//  Copyright © 2019 masato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
