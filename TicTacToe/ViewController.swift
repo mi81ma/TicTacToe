2e42302c6ecfece3756b7891f75412c810696f41
mi81ma <mi81ma@gmail.com>
2019-01-19 19:19:11 +0800

    make App

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -11,6 +11,17 @@ import UIKit
 class ViewController: UIViewController {
 
 
+    var turnCount = 0
+    var activeGame = true
+
+    // 1 is Orval, 2 is Cross
+    var activePlayer = 1
+
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]   // 0 - Empty, 1 - Orval, 2 -Cross
+
+    let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+
     var imageArray : [UIImage] = [#imageLiteral(resourceName: "Nothing"),#imageLiteral(resourceName: "Oval"),#imageLiteral(resourceName: "X")]
 
     var imageView = UIImageView()
@@ -24,6 +35,9 @@ class ViewController: UIViewController {
     var button07 = UIButton()
     var button08 = UIButton()
     var button09 = UIButton()
+
+    var winnerMessage = UILabel()
+    var playAgainButton = UIButton()
 
 
     override func viewDidLoad() {
@@ -41,144 +55,194 @@ class ViewController: UIViewController {
 
 
         button01.frame = CGRect(x: 0, y:  (self.view.frame.height - self.view.frame.width) / 2, width: imageView.bounds.width / 3, height: imageView.bounds.height / 3)
-        button01.setImage(imageArray[1], for: .normal)
+//        button01.setImage(imageArray[1], for: .normal)
         button01.addTarget(self, action: #selector(ViewController.onClickMyButton(sender:)), for: .touchUpInside)
         button01.tag = 1
         view.addSubview(button01)
 
         button02 = UIButton(frame: CGRect(x: imageView.bounds.width / 3, y:  (self.view.frame.height - self.view.frame.width) / 2, width: imageView.bounds.width / 3, height: imageView.bounds.height / 3))
-        button02.setImage(imageArray[1], for: .normal)
+//        button02.setImage(imageArray[1], for: .normal)
         button02.addTarget(self, action: #selector(ViewController.onClickMyButton(sender:)), for: .touchUpInside)
         button02.tag = 2
         view.addSubview(button02)
 
         button03 = UIButton(frame: CGRect(x: imageView.bounds.width / 3 * 2, y:  (self.view.frame.height - self.view.frame.width) / 2, width: imageView.bounds.width / 3, height: imageView.bounds.height / 3))
-        button03.setImage(imageArray[1], for: .normal)
+//        button03.setImage(imageArray[1], for: .normal)
         button03.addTarget(self, action: #selector(ViewController.onClickMyButton(sender:)), for: .touchUpInside)
         button03.tag = 3
         view.addSubview(button03)
 
 
         button04 = UIButton(frame: CGRect(x: 0, y: (self.view.frame.height - self.view.frame.width) / 2 + imageView.bounds.width / 3, width: imageView.bounds.width / 3, height: imageView.bounds.height / 3))
-        button04.setImage(imageArray[1], for: .normal)
+//        button04.setImage(imageArray[1], for: .normal)
         button04.addTarget(self, action: #selector(ViewController.onClickMyButton(sender:)), for: .touchUpInside)
         button04.tag = 4
         view.addSubview(button04)
 
         button05 = UIButton(frame: CGRect(x: imageView.bounds.width / 3, y: (self.view.frame.height - self.view.frame.width) / 2 + imageView.bounds.width / 3 , width: imageView.bounds.width / 3, height: imageView.bounds.height / 3))
-        button05.setImage(imageArray[1], for: .normal)
+//        button05.setImage(imageArray[1], for: .normal)
         button05.addTarget(self, action: #selector(ViewController.onClickMyButton(sender:)), for: .touchUpInside)
         button05.tag = 5
         view.addSubview(button05)
 
         button06 = UIButton(frame: CGRect(x: imageView.bounds.width / 3 * 2, y: (self.view.frame.height - self.view.frame.width) / 2 + imageView.bounds.width / 3, width: imageView.bounds.width / 3, height: imageView.bounds.height / 3))
-        button06.setImage(imageArray[1], for: .normal)
+//        button06.setImage(imageArray[1], for: .normal)
         button06.addTarget(self, action: #selector(ViewController.onClickMyButton(sender:)), for: .touchUpInside)
         button06.tag = 6
         view.addSubview(button06)
 
 
         button07 = UIButton(frame: CGRect(x: 0, y: (self.view.frame.height - self.view.frame.width) / 2 + imageView.bounds.width / 3 * 2, width: imageView.bounds.width / 3, height: imageView.bounds.height / 3))
-        button07.setImage(imageArray[1], for: .normal)
+//        button07.setImage(imageArray[1], for: .normal)
         button07.addTarget(self, action: #selector(ViewController.onClickMyButton(sender:)), for: .touchUpInside)
         button07.tag = 7
         view.addSubview(button07)
 
         button08 = UIButton(frame: CGRect(x: imageView.bounds.width / 3, y: (self.view.frame.height - self.view.frame.width) / 2 + imageView.bounds.width / 3 * 2, width: imageView.bounds.width / 3, height: imageView.bounds.height / 3))
-        button08.setImage(imageArray[1], for: .normal)
+//        button08.setImage(imageArray[1], for: .normal)
         button08.addTarget(self, action: #selector(ViewController.onClickMyButton(sender:)), for: .touchUpInside)
         button08.tag = 8
         view.addSubview(button08)
 
         button09 = UIButton(frame: CGRect(x: imageView.bounds.width / 3 * 2, y: (self.view.frame.height - self.view.frame.width) / 2 + imageView.bounds.width / 3 * 2, width: imageView.bounds.width / 3, height: imageView.bounds.height / 3))
-        button09.setImage(imageArray[1], for: .normal)
+//        button09.setImage(imageArray[1], for: .normal)
         button09.addTarget(self, action: #selector(ViewController.onClickMyButton(sender:)), for: .touchUpInside)
         button09.tag = 9
         view.addSubview(button09)
 
 
+
+
+        // Winner Message
+
+        winnerMessage.isHidden = true
+        winnerMessage.frame = CGRect(x: imageView.bounds.width / 6, y: self.view.frame.height / 2, width: imageView.bounds.width / 3 * 2, height: 100)
+        winnerMessage.textAlignment = .center
+        winnerMessage.center = CGPoint(x: winnerMessage.center.x - 500, y: winnerMessage.center.y - 100)
+        winnerMessage.textColor = UIColor.green
+        winnerMessage.font = winnerMessage.font.withSize(30)
+
+        view.addSubview(winnerMessage)
+
+
+        playAgainButton.isHidden = true
+        playAgainButton.frame = CGRect(x: imageView.bounds.width / 6, y: self.view.frame.height / 2, width: imageView.bounds.width / 3 * 2, height: 50)
+        playAgainButton.center = CGPoint(x: playAgainButton.center.x - 500, y: playAgainButton.center.y)
+        playAgainButton.setTitle("Play Again", for: [])
+        playAgainButton.addTarget(self, action: #selector(ViewController.onClickRepeatButton), for: .touchUpInside)
+
+        playAgainButton.backgroundColor = .red
+        playAgainButton.layer.cornerRadius = 10
+        winnerMessage.text = ""
+        playAgainButton.titleLabel?.font.withSize(60)
+        view.addSubview(playAgainButton)
+
     }
 
 
+
+
     /*
-     ボタンのイベント.
+     Button Action
      */
     @objc func onClickMyButton(sender: UIButton) {
+
+        turnCount += 1
+
+        let activePosition = sender.tag - 1
+
         print("onClickMyButton:")
         print("sender.tag \(sender.tag)")
+
+
+        if gameState[activePosition] == 0 {
+
+            gameState[activePosition] = activePlayer
+
+            if activePlayer == 1 {
+                sender.setImage(imageArray[1], for: [])
+                activePlayer = 2
+            } else {
+                sender.setImage(imageArray[2], for: [])
+                activePlayer = 1
+
+            }
+
+
+            // Win Judgement
+            for combination in winningCombinations {
+
+                if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
+
+                    // We have a winner!
+                    activeGame = false
+
+                    winnerMessage.isHidden = false
+                    playAgainButton.isHidden = false
+
+                    if gameState[combination[0]] == 1 {
+                        winnerMessage.text = "Noughts has won!"
+
+                    } else {
+                        winnerMessage.text = "Crosses has won!"
+                    }
+
+                    UIView.animate(withDuration: 1) {
+                        self.winnerMessage.center = CGPoint(x: self.winnerMessage.center.x + 500, y: self.winnerMessage.center.y)
+                        self.playAgainButton.center = CGPoint(x: self.playAgainButton.center.x + 500, y: self.playAgainButton.center.y)
+                    }
+
+                }
+            }
+
+            // Draw Judgement
+            if gameState.contains(0) == false {
+
+                print(gameState)
+
+                activeGame = false
+                winnerMessage.isHidden = false
+                playAgainButton.isHidden = false
+
+                winnerMessage.text = "This Game is Draw"
+
+                UIView.animate(withDuration: 1) {
+                    self.winnerMessage.center = CGPoint(x: self.winnerMessage.center.x + 500, y: self.winnerMessage.center.y)
+                    self.playAgainButton.center = CGPoint(x: self.playAgainButton.center.x + 500, y: self.playAgainButton.center.y)
+                }
+
+            }
+
+        }
+    }
+
+
+    @objc func onClickRepeatButton(sender: UIButton) {
+        print("Press Play again Button")
+
+        activeGame = true
+        activePlayer = 1
+        gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+
+
+        for i in 1..<10 {
+            if let button = view.viewWithTag(i) as? UIButton {
+                button.setImage(nil, for: [])
+            }
+             }
+
+
+
+            self.winnerMessage.center = CGPoint(x: self.winnerMessage.center.x - 500, y: self.winnerMessage.center.y)
+            self.playAgainButton.center = CGPoint(x: self.playAgainButton.center.x - 500, y: self.playAgainButton.center.y)
+
+            winnerMessage.isHidden = true
+            playAgainButton.isHidden = true
+
 
     }
 
 }
 
 
-
-//
-//import UIKit
-//
-//class ViewController: UIViewController {
-//
-//    private var myButton: UIButton!
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // Buttonを生成する.
-//        myButton = UIButton()
-//
-//        // ボタンのサイズ.
-//        let bWidth: CGFloat = 200
-//        let bHeight: CGFloat = 50
-//
-//        // ボタンのX,Y座標.
-//        let posX: CGFloat = self.view.frame.width/2 - bWidth/2
-//        let posY: CGFloat = self.view.frame.height/2 - bHeight/2
-//
-//        // ボタンの設置座標とサイズを設定する.
-//        myButton.frame = CGRect(x: posX, y: posY, width: bWidth, height: bHeight)
-//
-//        // ボタンの背景色を設定.
-//        myButton.backgroundColor = UIColor.red
-//
-//        // ボタンの枠を丸くする.
-//        myButton.layer.masksToBounds = true
-//
-//        // コーナーの半径を設定する.
-//        myButton.layer.cornerRadius = 20.0
-//
-//        // タイトルを設定する(通常時).
-//        myButton.setTitle("ボタン(通常)", for: .normal)
-//        myButton.setTitleColor(UIColor.white, for: .normal)
-//
-//        // タイトルを設定する(ボタンがハイライトされた時).
-//        myButton.setTitle("ボタン(押された時)", for: .highlighted)
-//        myButton.setTitleColor(UIColor.black, for: .highlighted)
-//
-//        myButton.setTitleColor(UIColor.green, for: .highlighted)
-//
-//        // ボタンにタグをつける.
-//        myButton.tag = 1
-//
-//        // イベントを追加する
-//        myButton.addTarget(self, action: #selector(ViewController.onClickMyButton(sender:)), for: .touchUpInside)
-//
-//        // ボタンをViewに追加.
-//        self.view.addSubview(myButton)
-//
-//    }
-//
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
-//
-//    /*
-//     ボタンのイベント.
-//     */
-//    @objc internal func onClickMyButton(sender: UIButton) {
-//        print("onClickMyButton:");
-//        print("sender.currentTitle: \(sender.currentTitle)")
-//        print("sender.tag: \(sender.tag)")
-//    }
-//
-//}
